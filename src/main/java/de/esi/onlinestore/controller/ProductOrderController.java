b7e9cc23be0c66ee15dacee045611864ad96bd72
Lidiia Shabaldina <lidiia.shabaldina@bearingpoint.com>
2021-04-20 16:11:42 +0200

    ProductOrder controller

--- src/main/java/de/esi/onlinestore/controller/ProductOrderController.java
+++ src/main/java/de/esi/onlinestore/controller/ProductOrderController.java
@@ -1,6 +1,5 @@
 package de.esi.onlinestore.controller;
 
-import de.esi.onlinestore.domain.Product;
 import de.esi.onlinestore.domain.ProductOrder;
 import de.esi.onlinestore.exceptions.BadRequestException;
 import de.esi.onlinestore.exceptions.ResourceNotFoundException;
@@ -24,7 +23,91 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class ProductOrderController {
 
+	private final Logger log = LoggerFactory.getLogger(ProductOrderController.class);
 
+	private final ProductOrderRepository productOrderRepository;
+
+	public ProductOrderController(ProductOrderRepository productOrderService) {
+
+		this.productOrderRepository = productOrderService;
+	}
+
+
+	//Get list of all product orders
+	@GetMapping("/orders")
+	public ResponseEntity<List<ProductOrder>> getAllProductOrders() {
+		log.info("REST request to get all orders");
+		List<ProductOrder> result = productOrderRepository.findAll();
+		return ResponseEntity.ok().body(result);
+	}
+
+	//Create new order
+	@PostMapping("/orders")
+	public ResponseEntity<ProductOrder> createProductOrder(@Valid @RequestBody ProductOrder productOrder) throws URISyntaxException, BadRequestException {
+		log.info("REST request to save order : {}", productOrder);
+		if ( productOrder.getId() != null) {
+			throw new BadRequestException("A new order cannot be added, ID exists");
+		}
+		ProductOrder result = productOrderRepository.save(productOrder);
+		return ResponseEntity.created(new URI("/api/orders/" + result.getId()))
+				.body(result);
+	}
+
+	//Get order by id
+	@GetMapping("/orders/{id}")
+	public ResponseEntity<ProductOrder> getProductOrder(@PathVariable Long id) throws ResourceNotFoundException {
+		log.info("REST request to get order : {}", id);
+		if(!productOrderRepository.existsById(id)){
+			throw new ResourceNotFoundException("id not found");
+		}
+		Optional<ProductOrder> order = productOrderRepository.findById(id);
+		ProductOrder result = order.get();
+		return ResponseEntity.ok()
+				.body(result);
+	}
+
+	//Overwrite order
+	@PutMapping("/orders")
+	public ResponseEntity<ProductOrder> updateProductOrder(@Valid @RequestBody ProductOrder productOrder)
+			throws BadRequestException, ResourceNotFoundException {
+		log.info("REST request to update order : {}", productOrder);
+		if (productOrder.getId() == null) {
+			throw new BadRequestException("Invalid id");
+		}
+		if(!productOrderRepository.existsById(productOrder.getId())){
+			throw new ResourceNotFoundException("id not found");
+		}
+		ProductOrder result = productOrderRepository.save(productOrder);
+		return ResponseEntity.ok()
+				.body(result);
+	}
+
+	//Overwrite order by id
+	@PutMapping("/orders/{id}")
+	public ResponseEntity<ProductOrder> updateProductOrder(@PathVariable Long id, @Valid @RequestBody ProductOrder productOrder)
+			throws BadRequestException, ResourceNotFoundException {
+		log.info("REST request to update order : {}", productOrder);
+		if (productOrder.getId() == null) {
+			throw new BadRequestException("Invalid id");
+		}
+		if(!productOrderRepository.existsById(id)){
+			throw new ResourceNotFoundException("id not found");
+		}
+		ProductOrder result = productOrderRepository.save(productOrder);
+		return ResponseEntity.ok()
+				.body(result);
+	}
+
+	//Delete order by id
+	@DeleteMapping("/orders/{id}")
+	public ResponseEntity<Void> deleteProductOrder(@PathVariable Long id) throws ResourceNotFoundException {
+		log.info("REST request to delete category : {}", id);
+		if(!productOrderRepository.existsById(id)){
+			throw new ResourceNotFoundException("id not found");
+		}
+		productOrderRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 }
 
 
